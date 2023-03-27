7a0241410ea73a67b9997eff4bfe4db6a40c25e2
scearpp <118109156+scearpp@users.noreply.github.com>
2023-03-28 02:39:15 +0800

    Add files via upload

---
+++ WineAll.bat
@@ -0,0 +1,8 @@
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
+sudo add-apt-repository ppa:lutris-team/lutris -y
+sudo apt update
+sudo apt-get install --install-recommends winehq-staging -y
+sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
+sudo apt-get install lutris -y
