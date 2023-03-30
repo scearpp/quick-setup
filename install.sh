31320d0e56f0a3b0b74cb7ca5d67b5ccb68acd4f
scearpp <118109156+scearpp@users.noreply.github.com>
2023-03-30 13:41:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
   exit 1
 fi
-
-# Change Debian to SID Branch
+T
 
 username=$(id -u -n 1000)
 builddir=$(pwd)
